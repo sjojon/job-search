0ef78d1444e1ea7a79a628447fc1555566980850
sjojon <90796535+sjojon@users.noreply.github.com>
2023-07-30 13:56:06 +0200

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+  root: true,
+  extends: [
+    "plugin:vue/vue3-recommended",
+    "eslint:recommended",
+    "@vue/eslint-config-prettier",
+    "plugin:vitest-globals/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
+  },
+  env: {
+    "vitest-globals/env": true,
+  },
+};
---
+++ src/components/ActionButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <button
+    class="rounded border-0 bg-brand-blue-1 px-5 py-3 font-medium text-white hover:shadow-blue"
+    @click="handleClick"
+  >
+    Sign in
+  </button>
+</template>
+
+<script>
+export default {
+  name: "ActionButton",
+  methods: {
+    handleClick() {
+      console.log("I've been clicked");
+    },
+  },
+};
+</script>
