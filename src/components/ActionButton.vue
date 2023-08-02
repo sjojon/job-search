9a9efc24544e53f0546c07c9e5ffd50d818ea631
sjojon <90796535+sjojon@users.noreply.github.com>
2023-08-02 13:49:28 +0200

    Begynt å forandre på knapper

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,13 +1,31 @@
 <template>
-  <button
-    class="rounded border-0 bg-brand-blue-1 px-5 py-3 font-medium text-white hover:shadow-blue"
-  >
-    Sign in
+  <button :class="buttonClass">
+    {{ text }}
   </button>
 </template>
 
 <script>
 export default {
   name: "ActionButton",
+  props: ["text", "isPrimary"],
+  computed: {
+    buttonClass() {
+      return { primary: this.isPrimary, secondary: !this.isPrimary };
+    },
+  },
 };
 </script>
+
+<style scoped>
+button {
+  @apply rounded px-5 py-3 font-medium;
+}
+
+.primary {
+  @apply border-0 bg-brand-blue-1 text-white hover:shadow-blue;
+}
+
+.secondary {
+  @apply bg-transparent text-brand-blue-1 hover:bg-brand-blue-2 hover:text-white;
+}
+</style>
