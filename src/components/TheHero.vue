25cd6c47f608cbf87c43a0261cdb9141b26ae122
sjojon <90796535+sjojon@users.noreply.github.com>
2023-08-10 11:12:42 +0200

    Begynt på overskrift/dynamiske ord med farge

---
+++ src/components/TheHero.vue
@@ -0,0 +1,28 @@
+<template>
+  <main>
+    <section class="flex h-screen flex-col pb-20 pt-10">
+      <div class="grid grid-cols-12">
+        <div class="col-span-1 col-start-1"></div>
+
+        <div class="col-span-5 col-start-2">
+          <the-headline />
+        </div>
+
+        <div class="col-span-5 col-start-7"></div>
+
+        <div class="col-span-1 col-start-12"></div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+import TheHeadline from "@/components/TheHeadline.vue";
+
+export default {
+  name: "TheHero",
+  components: {
+    TheHeadline,
+  },
+};
+</script>
